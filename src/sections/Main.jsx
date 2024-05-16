81ca746f01d42af7577023e5f96e1595e212e876
Diego G <d.g.g.87@hotmail.com>
2024-05-16 00:30:59 -0600

    organized to utils/handlers and component ProductForm

--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -1,11 +1,9 @@
 import {useEffect, useState} from 'react';
-import {v4} from 'uuid';
 import ProductCard from '../components/ProductCard';
+import ProductForm from '../components/ProductForm';
+import {handleDelete} from '../utils/handlers';
 
 const Main = () => {
-  const [newItem, setNewItem] = useState('');
-  const [newItemPrice, setNewItemPrice] = useState('');
-  const [newItemImage, setNewItemImage] = useState();
   const [productsList, setProductsList] = useState(() => {
     const storedProducts = localStorage.getItem('products');
     if (storedProducts === null) return [];
@@ -16,34 +14,6 @@ const Main = () => {
   useEffect(() => {
     localStorage.setItem('products', JSON.stringify(productsList));
   }, [productsList]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (newItem === '' || newItemPrice === '' || !newItemImage) return;
-    addItem(newItem, newItemPrice, newItemImage);
-    setNewItem('');
-    setNewItemPrice('');
-    setNewItemImage(null);
-  };
-
-  const addItem = (title, price, image) => {
-    setProductsList((currentList) => [
-      ...currentList,
-      {
-        id: v4(),
-        name: title,
-        price: price,
-        image: URL.createObjectURL(image),
-      },
-    ]);
-  };
-
-  const handleDelete = (id) => {
-    setProductsList((currentList) =>
-      currentList.filter((product) => product.id !== id)
-    );
-  };
 
   return (
     <section className="flex justify-evenly max-container max-sm:flex-col-reverse max-sm:p-2">
@@ -56,7 +26,9 @@ const Main = () => {
             <ProductCard
               key={product.id}
               {...product}
-              handleDelete={handleDelete}
+              handleDelete={() =>
+                handleDelete(product.id, productsList, setProductsList)
+              }
             />
           ))}
         </div>
@@ -65,39 +37,7 @@ const Main = () => {
         <div className="mb-4">
           <h2 className="font-VT323 text-2xl">AGREGAR PRODUCTO:</h2>
         </div>
-        <form
-          className="flex flex-col items-center w-[60%]"
-          onSubmit={handleSubmit}
-        >
-          <input
-            value={newItem}
-            onChange={(e) => setNewItem(e.target.value)}
-            type="text"
-            placeholder="nombre..."
-            className="input"
-            required
-          />
-          <input
-            value={newItemPrice}
-            pattern="[0-9]*\.?[0-9]*"
-            onChange={(e) => setNewItemPrice(e.target.value)}
-            type="text"
-            placeholder="precio..."
-            className="input"
-            required
-          />
-          <input
-            type="file"
-            accept="image/*"
-            onChange={(e) => setNewItemImage(e.target.files[0])}
-            className="input"
-          />
-          <div className="flex w-[60%] justify-center max-sm:justify-evenly">
-            <button className="px-5 py-1 rounded-md bg-primary-blue text-white">
-              Agregar
-            </button>
-          </div>
-        </form>
+        <ProductForm setProductsList={setProductsList} />
       </div>
     </section>
   );
